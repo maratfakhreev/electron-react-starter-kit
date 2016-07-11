71789412ba8c65d22c089e9f93075d0c43ab128d
Marat Fakhreev <marat.fakhreev@flatstack.com>
2016-07-11 08:32:28 +0300

    Make minor design fixes

--- app/components/home/index.jsx
+++ app/components/home/index.jsx
@@ -4,7 +4,7 @@ export default class Home extends React.Component {
   render() {
     return (
       <section className="home">
-        <h1>Electron-react starter kit</h1>
+        <h1 className="home-title">Electron-react starter kit</h1>
       </section>
     );
   }
